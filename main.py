8c47a9fdfc9d9ce4f63ef1d754ec73f9fd5aaee2
VenkataBhaskarr <venkatabhaskarcodes@gmail.com>
2023-04-14 10:36:07 +0530

    added initial file

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("creating a fuckin static but fckin amazing website excited lesgo")
+
+
+st.multiselect('Buy', ['milk', 'apples', 'potatoes'])
+
+
+st.text_area('Text to translate')
+
