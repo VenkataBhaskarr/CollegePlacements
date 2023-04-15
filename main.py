b0983471b74d68ae95aad9f00273f340de16cffa
VenkataBhaskar <111889155+VenkataBhaskarr@users.noreply.github.com>
2023-04-15 12:24:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.divider()
 st.info("# Company A\n\nDate of the mail recieved: 12-04-2023\n\nDeadline to apply: 12-04-2023\n\n#### Eligibility criteria : B.techCSE\n\n#### Previously asked questions : [StackEdit](https://youtube.com/)\n\n#### About the company : [CompanyA](https://youtube.com/)")
 
 st.divider()
-st.error("# Information\nThis is some pre-defined text.") 
+st.error("# Information\nThis is not some pre-defined text.") 
 st.divider()
 st.success("# Information\nThis is some pre-defined text.")
 st.divider()
